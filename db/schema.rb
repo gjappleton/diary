dcf456452a637ebd639d6fedb3600a256000516c
gabe <gabrieljappleton@gmail.com>
2016-11-19 11:43:53 -0500

    fixed show and index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161119153855) do
+ActiveRecord::Schema.define(version: 20161119162400) do
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "body"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
