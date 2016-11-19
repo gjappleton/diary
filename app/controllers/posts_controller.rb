4e6fbf894ea90d87a8f79f84403f6cb3a43a5bcd
gabe <gabrieljappleton@gmail.com>
2016-11-19 11:46:41 -0500

    quick fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     respond_to do |format|
-      if @post.save
+      if @post.save!
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
