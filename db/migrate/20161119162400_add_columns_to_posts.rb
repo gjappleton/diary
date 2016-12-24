e24365270dfa0a7f2bf8b872211c1eb7f5922361
gabe <gabrieljappleton@gmail.com>
2016-12-24 15:20:49 -0500

    fixed wonky migrations, fresh DB

--- db/migrate/20161119162400_add_columns_to_posts.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToPosts < ActiveRecord::Migration[5.0]
-  def change
-    add_column :posts, :title, :string
-    add_column :posts, :body, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -28,11 +28,11 @@ ActiveRecord::Schema.define(version: 20161208003115) do
   end
 
   create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.string   "title"
-    t.text     "body"
   end
 
   create_table "users", force: :cascade do |t|
