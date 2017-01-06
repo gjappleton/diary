ad416f5583d35345d055b4e764b9b0feb1a27fa9
gabe <gabrieljappleton@gmail.com>
2017-01-06 18:44:23 -0500

    asd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,9 @@ class UsersController < ApplicationController
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -21,4 +24,5 @@ class UsersController < ApplicationController
     params.require(:user).permit(:email, :password,
                                  :password_confirmation, :friend_id)
   end
+end
 end
